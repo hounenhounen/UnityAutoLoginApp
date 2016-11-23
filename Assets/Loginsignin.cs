e64ec2497a6da1b254b754f14cf0f999d2f0bfea
hounenhounen <kawahara@kawahara-no-MacBook-Air-9.local>
2016-11-23 21:42:47 +0900

    commit
    First

---
+++ Assets/Loginsignin.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using NCMB;
+
+//private InputField UserName;
+//private InputField PassWord;
+
+public class Loginsignin : MonoBehaviour
+{
+	public InputField UserName;
+	public InputField PassWord;
+	// Use this for initialization
+	void Start ()
+	{
+		if (PlayerPrefs.GetString ("Id") == "" && PlayerPrefs.GetString ("PW") == "") {
+			Dictionary<string, string> accountData = new Dictionary<string, string> ();
+			accountData = GenerateIdPW ();
+			UserName.text = accountData ["Id"];
+			PassWord.text = accountData ["PW"];
+			UnityEditor.EditorUtility.DisplayDialog("AutoAcount", "Generate Id&PW", "OK");
+		} else {
+			UserName.text = PlayerPrefs.GetString ("Id");
+			PassWord.text = PlayerPrefs.GetString ("PW");
+			UnityEditor.EditorUtility.DisplayDialog("AutoAcount", "Read Id&PW From PlayerPrefs", "OK");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Login ()
+	{
+		print (UserName.text);
+		print (PassWord.text);
+
+		//NCMBUserのインスタンス作成 
+		NCMBUser user = new NCMBUser ();
+
+		// ユーザー名とパスワードでログイン
+		NCMBUser.LogInAsync (UserName.text, PassWord.text, (NCMBException e) => {    
+			if (e != null) {
+				UnityEngine.Debug.Log ("ログインに失敗: " + e.ErrorMessage);
+			} else {
+				UnityEngine.Debug.Log ("ログインに成功！");
+				Application.LoadLevel ("LogOut");
+			}
+		});
+
+	}
+
+	public void Signin ()
+	{
+		print (UserName.text);
+		print (PassWord.text);
+
+
+		//NCMBUserのインスタンス作成 
+		NCMBUser user = new NCMBUser ();
+		
+		//ユーザ名とパスワードの設定
+		user.UserName = UserName.text;
+		user.Password = PassWord.text;
+		
+		//会員登録を行う
+		user.SignUpAsync ((NCMBException e) => { 
+			if (e != null) {
+				UnityEngine.Debug.Log ("新規登録に失敗: " + e.ErrorMessage);
+			} else {
+				UnityEngine.Debug.Log ("新規登録に成功");
+				PlayerPrefs.SetString ("Id",UserName.text);
+				PlayerPrefs.SetString ("PW",PassWord.text);
+				PlayerPrefs.Save();
+				Application.LoadLevel ("LogOut");
+			}
+		});
+
+	}
+
+	public Dictionary<string,string> GenerateIdPW(){
+		Dictionary<string, string> accountData = new Dictionary<string, string> ();
+		System.Guid idGuid=System.Guid.NewGuid();
+		System.Guid pwGuid=System.Guid.NewGuid();
+		string Id_uuid=idGuid.ToString();
+		string PW_uuid=pwGuid.ToString();
+		accountData.Add ("Id",Id_uuid);
+		accountData.Add ("PW",PW_uuid);
+		return accountData;
+	}
+
+	public void DeletePlayerPrefs(){
+		PlayerPrefs.DeleteAll();
+		UserName.text = "";
+		PassWord.text = "";
+		Debug.Log("Delete All Data Of PlayerPrefs!!");
+	}
+}
